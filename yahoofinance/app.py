2ed99eb2ebd58d0418a75957698fd6d51893f386
kush-hbk <kush.shah@hbkworlc.com>
2022-09-02 04:58:57 +0530

    yahoo-finance: phase-2: refactoring

--- yahoofinance/app.py
+++ yahoofinance/app.py
@@ -1,24 +1,16 @@
 import pandas as pd
 import numpy as np
-# import warnings
-import bs4 as bs
-import requests
 import streamlit as st
 
 from income_statement.income_statement import income_statement
 from file_management.write_operations import write_file
+from file_management.read_operations import web_scrapper
 
 
 if __name__ == "__main__":
     ticker = 'TSLA'
 
-    # ## Read the yahoo income statement url and assign URL
-    income_url = f'https://finance.yahoo.com/quote/{ticker}/financials?p={ticker}'
-
-    read_income_url = requests.get(income_url, headers={'User-Agent': 'Custom'})
-
-    # BeautifulSoup the xml
-    income_soup = bs.BeautifulSoup(read_income_url.text, "lxml")
+    income_soup = web_scrapper(ticker)
     
     # create a file and write the xml/html file extracted by BeautifulSoup
     write_file("income_soup", str(income_soup))
--- yahoofinance/file_management/write_operations.py
+++ yahoofinance/file_management/write_operations.py
@@ -1,5 +1,19 @@
+# import os
+
+# def write_file(name, content):
+#     ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__)))
+#     relative_directory = os.path.join(ROOT_DIR, 'data', 'results')
+#     f = open(relative_directory +'/' + "{}.txt".format(name), "w+")
+#     f.write(content)
+#     f.close()
+#     return
+
+# import shutil
+# shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
+
 def write_file(name, content):
-    f = open("{}.txt".format(name), "w")
+    filename = "{}.txt".format(name)
+    f = open(filename, "w")
     f.write(content)
     f.close()
     return
